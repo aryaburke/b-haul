c0d9f2fc510679763a859e288ae1de8674b22850
arya <aryaburke4@gmail.com>
2021-08-04 23:11:13 -0400

    backend functions working

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -1,45 +1,91 @@
+const { Database } = require('sqlite3');
+
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('database.db');
 
 //YYYY-MM-DD HH:MM
 function initialize() {
-    //creating tables
-    var initquery1 = `CREATE TABLE IF NOT EXISTS Trucks(
-        id INTEGER PRIMARY KEY NOT NULL,
-        model TEXT
-    )`;
-    db.run(initquery1);
-    var initquery2 = `CREATE TABLE IF NOT EXISTS Reservations(
-        id INTEGER PRIMARY KEY NOT NULL,
-        truck_id INTEGER,
-        start TEXT,
-        end TEXT,
-        name TEXT,
-        FOREIGN KEY truck_id REFERENCES Trucks(id)
-    )`
-    db.run(initquery2);
+    //creating tables and their schemas if they don't already exist
+    db.serialize(function(){
+        db.run(`CREATE TABLE IF NOT EXISTS Trucks(
+            id INTEGER PRIMARY KEY NOT NULL,
+            model TEXT
+        )`);
+        db.run(`CREATE TABLE IF NOT EXISTS Reservations(
+            id INTEGER PRIMARY KEY NOT NULL,
+            truck_id INTEGER,
+            start TEXT,
+            end TEXT,
+            name TEXT,
+            FOREIGN KEY (truck_id) REFERENCES Trucks(id)
+        )`);
+    });
+}
 
-    //create mock data to display on site
+function clear() {
+    //this function clears all tables from the database
+    db.run("DROP TABLE Trucks; DROP TABLE Reservations");
+}
 
-    //truck data
-    db.run("INSERT INTO Trucks VALUES (1, Van)")
-    db.run("INSERT INTO Trucks VALUES (2, 18')")
-    db.run("INSERT INTO Trucks VALUES (3, 24')")
-
-    //job data
+function add(table, val) {
+    //this is used to add entries to one of the tables. it also does checking to see if the proper data is given
+    //val is a list of the values in column order
+    //would be much more robust with serialization
+    var stmt;
+    var err;
+    if (table === "Trucks") {
+        if (val.length === 2) {
+            stmt = db.prepare("INSERT INTO Trucks VALUES (?,?)");
+            stmt.run(val[0],val[1]);
+            stmt.finalize();
+        } else {
+            err = `Table ${table} requires 2 values, ${val.length} were provided`;
+            throw err;
+        }
+    } else if (table === "Reservations") {
+        if (val.length === 5) {
+            stmt = db.prepare("INSERT INTO Reservations (?,?,?,?,?)")
+            stmt.run(val[0],val[1],val[2],val[3],val[4]);
+            stmt.finalize();
+        } else {
+            err = `Table ${table} requires 5 values, ${val.length} were provided`;
+            throw err;
+        }
+    } else {
+        err = `Table ${table} does not exist.`;
+        throw err;
+    }
 
 }
 
-/*db.serialize(function() {
-  db.run(`CREATE TABLE lorem (info TEXT)`);
+function populate() {
+    //this function is used to populate the database file with mock data
+    //truck data
+    var table = "Trucks";
+    var data = [
+        [1, "Van"],
+        [2, "18 ft"],
+        [3, "24 ft"],
+    ]
+    data.forEach(function (val) { 
+        add(table, val);
+    });
+}
 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
+function test() {
+    //!test function
+    clear();
+    initialize();
+    populate();
+    db.each("SELECT * FROM Trucks", function(err, row) {
+        console.log(row.id + ": " + row.model);
+        if (err){
+            console.log(err);
+        }
+    });
+    db.close();
 
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});*/
+}
+
+test();
+
